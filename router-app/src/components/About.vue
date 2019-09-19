213de4008f39d62da7fea8a4f3fe5ccf4d7e5078
Joonas Lundwall <joonas.lundwall@gmail.com>
2019-09-19 23:18:08 +0100

    First commit

---
+++ router-app/src/components/About.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="about">
+    <h1>{{ msg }}</h1>
+    <h1>{{ msg2 }}</h1>
+    <h1>{{ msg3 }}</h1>
+    <h2>{{ msg4 }}</h2>
+    <h3>{{ msg5 }}</h3>
+    <h2>{{ msg6 }}</h2>
+    <h3>{{ msg7 }}</h3>
+    <h2>{{ msg8 }}</h2>
+    <h3>{{ msg9 }}</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Reports',
+    data () {
+      return {
+        msg: 'Kmom01 - first me site.',
+        msg3: "##Build Setup",
+        msg4: "install dependencies:",
+        msg5: "npm install",
+        msg6: "serve with hot reload at localhost:8080:",
+        msg7: "npm run dev",
+        msg8: "build for production with minification:",
+        msg9: "npm run build"
+      }
+    }
+  }
+</script>
---
+++ router-app/src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h3>{{brodtext}}</h3>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Hello',
+    data () {
+      return {
+        msg: 'Min Me-app!',
+        brodtext: "Hejsan. Här är min blygsamma me-app, skapad med Vue. Jag hoppas kunna göra den finare i senare kmom."
+      }
+    }
+  }
+</script>
